8f0b0cf1a3ebac058eeff1ba89207379e662f45b
Nick <ndanvers222@gmail.com>
2020-01-08 03:12:02 +0000

    eslinted and refurbished

--- d2-focal/password.js
+++ d2-focal/password.js
@@ -1,28 +1,27 @@
 const args = process.argv.splice(2);
-
-const obfuscate = function (string) {
-    obfuscatedString = "";
-    let newString = string.toString();
-    for(let i = 0; i < newString.length; i ++) {
-        switch(newString[i]) {
-            case "a":
-            obfuscatedString += "4";
-            break;
-            case "e":
-            obfuscatedString += "3";
-            break;
-            case "o":
-            obfuscatedString += "0";
-            break;
-            case "l":
-            obfuscatedString += "1";
-            break;
-            default: 
-            obfuscatedString += newString[i];
-            break;
-        }
+//
+const obfuscate = function(string) {
+  let newString = string.toString();
+  let obfuscatedString = "";
+  for (let i = 0; i < newString.length; i ++) {
+    switch (newString[i]) {
+    case "a":
+      obfuscatedString += "4";
+      break;
+    case "e":
+      obfuscatedString += "3";
+      break;
+    case "o":
+      obfuscatedString += "0";
+      break;
+    case "l":
+      obfuscatedString += "1";
+      break;
+    default:
+      obfuscatedString += newString[i];
+      break;
     }
-    return obfuscatedString;
-}
-
+  }
+  return obfuscatedString;
+};
 console.log(obfuscate(args));
